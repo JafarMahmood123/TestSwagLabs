b716f3628c78e93340c058f931dacc8121a73b9e
JafarMahmood123 <gafarARC.com@gmail.com>
2026-01-04 17:23:00 +0300

    feat: enhance OverViewPage with navigation methods and total price calculations, add comprehensive tests for navigation and price validation

--- TestSwagLabs/Pages/OverViewPage.cs
+++ TestSwagLabs/Pages/OverViewPage.cs
@@ -1,8 +1,62 @@
-using System;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V141.Debugger;
 
 namespace TestSwagLabs.Pages;
 
 public class OverViewPage
 {
+    private readonly IWebDriver _driver;
 
+    public OverViewPage(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+
+    public void NavigateBackToInventoryPage()
+    {
+        var finishButton = _driver.FindElement(By.Id("cancel"));
+        finishButton.Click();
+    }
+
+    public void NavigateToCompletePage()
+    {
+        var finishButton = _driver.FindElement(By.Id("finish"));
+        finishButton.Click();
+    }
+
+    public void NavigateToItemPageBasedOnItemId(string itemId)
+    {
+        var itemLink = _driver.FindElement(By.Id(itemId));
+        itemLink.Click();
+    }
+
+    public double CalculateTotalPrice()
+    {
+        double totalPrice = 0.0;
+        var cartItems = _driver.FindElements(By.ClassName("cart_item"));
+        foreach (var cartItem in cartItems)
+        {
+            var item = cartItem.FindElement(By.ClassName("inventory_item_price"));
+            var itemsQuantity = cartItem.FindElement(By.ClassName("cart_quantity"));
+
+            var quantity = int.Parse(itemsQuantity.Text);
+            var priceText = double.Parse(item.Text.Replace("$", ""));
+            totalPrice += priceText * quantity;
+        }
+
+        var tax = _driver.FindElement(By.ClassName("summary_tax_label"));
+        var taxValueText = tax.Text.Replace("Tax: $", "");
+        totalPrice += double.Parse(taxValueText);
+
+        return totalPrice;
+    }
+
+    public double GetDisplayedTotalPrice()
+    {
+        var totalElement = _driver.FindElement(By.ClassName("summary_total_label"));
+        var totalText = totalElement.Text.Replace("Total: $", "");
+        return double.Parse(totalText);
+    }
 }
--- TestSwagLabs/Tests/OverViewTests.cs
+++ TestSwagLabs/Tests/OverViewTests.cs
@@ -1,8 +1,105 @@
-using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using TestSwagLabs.Pages;
 
 namespace TestSwagLabs.Tests;
 
 public class OverViewTests
 {
+    OpenQA.Selenium.IWebDriver driver;
 
+    [SetUp]
+    public void Setup()
+    {
+        var options = new ChromeOptions();
+        options.AddArgument("--headless");
+
+        // These are useful for stability on Linux/Containers
+        options.AddArgument("--width=1920");
+        options.AddArgument("--height=1080");
+
+        // Initialize the Chrome Driver
+        driver = new ChromeDriver(options);
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        driver.Dispose();
+    }
+
+    [Test]
+    [TestCase("standard_user", "secret_sauce")]
+    public void NavigateToCompletePage_ShouldNavigateToOrderCompletePage(string userName, string password)
+    {
+        driver.Navigate().GoToUrl(SiteUrls.SiteUrl);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.Login(userName, password);
+
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        inventoryPage.NavigateToCartItemsPage();
+
+        CartPage cartPage = new CartPage(driver);
+        cartPage.NavigateToCheckOutPage();
+
+        ChekOutPage chekOutPage = new ChekOutPage(driver);
+        chekOutPage.EnterCheckoutInformation("Jafar", "Mahmood", "12345");
+        chekOutPage.NavigateToOverViewPage();
+
+        OverViewPage overViewPage = new OverViewPage(driver);
+        overViewPage.NavigateToCompletePage();
+
+        Assert.That(driver.Url, Is.EqualTo(SiteUrls.OrderCompleteUrl));
+    }
+
+    [Test]
+    [TestCase("standard_user", "secret_sauce")]
+    public void NavigateBackToInventoryPage_ShouldNavigateToInventoryPage(string userName, string password)
+    {
+        driver.Navigate().GoToUrl(SiteUrls.SiteUrl);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.Login(userName, password);
+
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        inventoryPage.NavigateToCartItemsPage();
+
+        CartPage cartPage = new CartPage(driver);
+        cartPage.NavigateToCheckOutPage();
+
+        ChekOutPage chekOutPage = new ChekOutPage(driver);
+        chekOutPage.EnterCheckoutInformation("Jafar", "Mahmood", "12345");
+        chekOutPage.NavigateToOverViewPage();
+
+        OverViewPage overViewPage = new OverViewPage(driver);
+        overViewPage.NavigateBackToInventoryPage();
+
+        Assert.That(driver.Url, Is.EqualTo(SiteUrls.InventoryUrl));
+    }
+
+    [Test]
+    [TestCase("standard_user", "secret_sauce", 2)]
+    public void CalculateTotalPrice_ShouldReturnTotalPriceIncludingTax(string userName, string password, int numberOfItemsToBeAdded)
+    {
+        driver.Navigate().GoToUrl(SiteUrls.SiteUrl);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.Login(userName, password);
+
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        inventoryPage.AddNItemsToCart(numberOfItemsToBeAdded);
+        inventoryPage.NavigateToCartItemsPage();
+
+        CartPage cartPage = new CartPage(driver);
+        cartPage.NavigateToCheckOutPage();
+
+        ChekOutPage chekOutPage = new ChekOutPage(driver);
+        chekOutPage.EnterCheckoutInformation("Jafar", "Mahmood", "12345");
+        chekOutPage.NavigateToOverViewPage();
+
+        OverViewPage overViewPage = new OverViewPage(driver);
+        double totalPrice = overViewPage.CalculateTotalPrice();
+
+        double expectedTotalPrice = overViewPage.GetDisplayedTotalPrice();
+
+        Assert.That(totalPrice, Is.EqualTo(expectedTotalPrice));
+    }
 }
